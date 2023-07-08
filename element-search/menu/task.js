792b14cb38f53b1a25fcceaf0b911d80009f98ad
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-07-09 00:44:14 +0300

    second commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,4 +1,7 @@
 let OpenMenueArray = Array.from(document.querySelectorAll(".menu__link"))
+const activeLink = document.querySelectorAll('.menu_sub');
+OpenMenueArray.filter(el => el !== activeLink).forEach(el => el.classList.remove("menu_active"))
+OpenMenueArray.filter(el => el == activeLink).forEach(el => el.classList.add("menu_active"))
 for (let el of OpenMenueArray) {
 	el.onclick = function() {
 		let mainEl = el.parentElement;
@@ -13,6 +16,8 @@ for (let el of OpenMenueArray) {
 		}
 	}
 }
+
+
 
 
 
