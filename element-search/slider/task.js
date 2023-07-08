8cb52aa42567f93041b2f6734ede8ad398d741ac
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-07-09 01:06:44 +0300

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,15 +2,17 @@ let slides = Array.from(document.querySelectorAll(".slider__item"));
 let arrowPrev = document.querySelector(".slider__arrow_prev");
 let arrowNext = document.querySelector(".slider__arrow_next");
 
-let index = 0;
+
 
 function nextSlide() {
+	let index = slides.findIndex((el) => el.classList.contains('slider__item_active'))
     slides[index].classList.remove('slider__item_active');
     index = (index + 1) % slides.length;
     slides[index].classList.add('slider__item_active');
 }
 
 function prevSlide() {
+	let index = slides.findIndex((el) => el.classList.contains('slider__item_active'))
     slides[index].classList.remove('slider__item_active');
     index = ((index - 1) + slides.length) % slides.length;
     slides[index].classList.add('slider__item_active');
