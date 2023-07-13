16a742f6fc559b15cce6d002067fd967b58249bb
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-07-13 22:50:32 +0300

    third commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,23 +1,25 @@
-let OpenMenueArray = Array.from(document.querySelectorAll(".menu__link"))
+let openMenueArray = Array.from(document.querySelectorAll(".menu__link"))
 const activeLink = document.querySelectorAll('.menu_sub');
-OpenMenueArray.filter(el => el !== activeLink).forEach(el => el.classList.remove("menu_active"))
-OpenMenueArray.filter(el => el == activeLink).forEach(el => el.classList.add("menu_active"))
-for (let el of OpenMenueArray) {
+openMenueArray.filter(el => el !== activeLink).forEach(el => el.classList.remove("menu_active"))
+openMenueArray.filter(el => el == activeLink).forEach(el => el.classList.add("menu_active"))
+
+for (let el of openMenueArray) {
 	el.onclick = function() {
 		let mainEl = el.parentElement;
-		if (mainEl.querySelector('.menu_sub').className === 'menu menu_sub') {
-			mainEl.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
-		} else {
-			mainEl.querySelector('.menu_sub').className = 'menu menu_sub'
-		}
+		let sub = mainEl.querySelector('.menu_sub');
+		if (sub) {
+			if (sub.className === 'menu menu_sub') {
+				sub.className = 'menu menu_sub menu_active';
+			} else {
+				sub.className = 'menu menu_sub';
+			}
 
-		if (el.closest('.menu__item')) {
-			return false;
+			if (el.closest('.menu__item')) {
+				return false;
+			}
 		}
 	}
 }
-
-
 
 
 
