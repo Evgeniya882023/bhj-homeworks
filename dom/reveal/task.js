a795c163b40dbccc9ff56fadfe55eaa0066ad663
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-08-01 00:01:08 +0300

    second commit reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -3,9 +3,10 @@ const revealArray = Array.from(document.querySelectorAll('.reveal'));
 function activeReveal() {
 	for (let el of revealArray) {
 		el.classList.remove('reveal_active');
-		if (el.getBoundingClientRect().top < window.innerHeight) {
+		if (el.getBoundingClientRect().bottom <= window.innerHeight && el.getBoundingClientRect().top < window.innerHeight && el.getBoundingClientRect().bottom >= 0) {
 			el.classList.add('reveal_active');
 		}
+		
 	}
 }
 document.addEventListener('scroll', activeReveal);
