08bd48877b80d0c2830c03fb1d1dc36dce12e3ca
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-08-21 22:29:10 +0300

    first commit progressbar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,14 @@
+const progress = document.getElementById("progress");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+   const xhr = new XMLHttpRequest();
+   const formData = new FormData(form);
+   xhr.upload.addEventListener("progress", function (e) {
+      progress.value = e.loaded / e.total;
+
+   })
+   xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
+   xhr.send(formData);
+})
